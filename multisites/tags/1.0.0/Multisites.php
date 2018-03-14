28e48989e75ec820d84fc4b207ba5f08bcb161d4
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-03-14 09:38:57 +0000

    presstify-plugins/multisites tag 1.0 dépot initial

---
+++ multisites/tags/1.0.0/Multisites.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ Plugin Name: Multisites
+ Plugin URI: https://presstify.com/plugins/multisites
+ Description: Multisites
+ Version: 1.0.0
+ Author: Milkcreation
+ Author URI: http://milkcreation.fr
+ Text Domain: tify
+*/
+
+namespace tiFy\Plugins\Multisites;
+
+class Multisites extends \tiFy\App\Plugin
+{
+    /**
+     * CONSTRUCTEUR
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // Déclaration des événement de déclenchement
+        $this->tFyAppActionAdd('admin_enqueue_scripts');
+        $this->tFyAppActionAdd('user_new_form');
+    }
+
+    /**
+     * DECLENCHEURS
+     */
+    /**
+     * Mise en file de scripts de l'interface d'administration
+     *
+     * @return void
+     */
+    public function admin_enqueue_scripts()
+    {
+        wp_enqueue_script('tiFyPluginsMultisites', self::tFyAppUrl() . '/assets/js/Multisites.js', ['jquery'], 171106);
+    }
+
+    /**
+     * Ajout de champs de pour la création de nouveaux utilisateurs
+     *
+     * @param string $context
+     *
+     * @return string
+     */
+    public function user_new_form($context)
+    {
+        if (!is_multisite()|| !current_user_can('manage_network_users')) :
+            return;
+        endif;
+        if (!in_array($context, ['add-existing-user', 'add-new-user'])) :
+            return;
+        endif;
+
+        //Force la création d'un nouvel utilisateur sans demande de confirmation par email
+?>
+<input type="hidden" name="noconfirmation" value="1" />
+<?php
+    }
+}
