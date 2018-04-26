a74e658912eb270d54175a73d3bb31856f8f6a21
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-04-26 12:28:16 +0000

    multisites -- branch 1.1 -- création

---
+++ multisites/branches/1.1/assets/js/Multisites.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function($){
+    $('#noconfirmation, #adduser-noconfirmation').prop('checked', true);
+    $('#noconfirmation, #adduser-noconfirmation').prop('disabled', true);
+});
