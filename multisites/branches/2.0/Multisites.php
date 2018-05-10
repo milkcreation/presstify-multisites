f748e39c6ee6bf094949633c7a3cf820eb5d5b8b
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-05-10 16:39:01 +0000

    multisites -- branch2.0 -- Migration PresstiFy v2

---
+++ multisites/branches/2.0/Multisites.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @name Multisites
+ * @desc Extension PresstiFy de gestion de Wordpress multisites.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package presstiFy
+ * @namespace \tiFy\Plugins\Multisites
+ * @version 2.0.0
+ */
+
+namespace tiFy\Plugins\Multisites;
+
+use tiFy\Apps\AppController;
+
+final class Multisites extends AppController
+{
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Déclaration des événement de déclenchement
+        $this->appAddAction('admin_enqueue_scripts');
+        $this->appAddAction('user_new_form');
+    }
+
+    /**
+     * Mise en file de scripts de l'interface d'administration.
+     *
+     * @return void
+     */
+    public function admin_enqueue_scripts()
+    {
+        \wp_enqueue_script('tiFyPluginsMultisites', $this->appUrl() . '/assets/js/Multisites.js', ['jquery'], 171106);
+    }
+
+    /**
+     * Ajout de champs de pour la création de nouveaux utilisateurs.
+     *
+     * @param string $context
+     *
+     * @return string
+     */
+    public function user_new_form($context)
+    {
+        if (!is_multisite()|| !current_user_can('manage_network_users')) :
+            return;
+        endif;
+        if (!in_array($context, ['add-existing-user', 'add-new-user'])) :
+            return;
+        endif;
+
+        //Force la création d'un nouvel utilisateur sans demande de confirmation par email
+?><input type="hidden" name="noconfirmation" value="1" /><?php
+    }
+}
