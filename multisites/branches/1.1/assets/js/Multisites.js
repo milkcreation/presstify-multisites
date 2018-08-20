547a64ce5435378373e93e6280f859b6addc1e9d
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-08-20 11:06:55 +0000

    multisites -- version obsolete tiFy 1.5

--- multisites/branches/1.1/assets/js/Multisites.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function($){
-    $('#noconfirmation, #adduser-noconfirmation').prop('checked', true);
-    $('#noconfirmation, #adduser-noconfirmation').prop('disabled', true);
-});
