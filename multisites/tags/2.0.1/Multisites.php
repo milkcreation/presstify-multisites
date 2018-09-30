ac146432606a19c315f735f3d8e2e56b0810dddc
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-09-30 10:16:36 +0000

    multisites -- tag 2.0.1 -- adaptation réécriture tiFy 2.0

---
+++ multisites/tags/2.0.1/Multisites.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @name Multisites
+ * @desc Extension PresstiFy de gestion de Wordpress multisites.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package presstify-plugins/multisites
+ * @namespace \tiFy\Plugins\Multisites
+ * @version 2.0.1
+ */
+
+namespace tiFy\Plugins\Multisites;
+
+final class Multisites
+{
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        add_action(
+            'admin_enqueue_scripts',
+            function () {
+                wp_enqueue_script(
+                    'multisites',
+                    class_info($this)->getUrl() . '/Resources/assets/js/scripts.js',
+                    ['jquery'],
+                    171106
+                );
+            }
+        );
+
+        add_action(
+            'user_new_form',
+            function ($context) {
+                if (!is_multisite()|| !current_user_can('manage_network_users')) :
+                    return;
+                elseif (!in_array($context, ['add-existing-user', 'add-new-user'])) :
+                    return;
+                endif;
+
+                //Force la création d'un nouvel utilisateur sans demande de confirmation par email.
+                ?><input type="hidden" name="noconfirmation" value="1" /><?php
+            }
+        );
+    }
+}
